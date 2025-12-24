956fea594b8450f989955f839b04caba9350073e
leogao <leogao2006@gmail.com>
2025-12-24 13:19:02 -0500

    4

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -13,11 +13,12 @@ export default function Calendar({ onSelectDoor }) {
         {title.split('').map((char, i) => (
           <motion.span
             key={i}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
             transition={{
-              delay: i * 0.06,
-              duration: 0.1,
+              delay: 0.3 + i * 0.05,
+              duration: 0.8,
+              ease: [0.25, 0.1, 0.25, 1],
             }}
           >
             {char}
